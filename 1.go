b4a533e8bf32d28647e75ee61eae7d0bea24a7df
Luv <luv.vermani@gmail.com>
2024-12-02 03:04:46 +0530

    made code easier to understand

--- 1.go
+++ 1.go
@@ -3,24 +3,66 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
 )
 
+// Since bufioReader has no close on it
+// creating this type to help close the file in the calling function
+type bufioCloser struct {
+	*bufio.Reader
+	io.Closer
+}
+
+/*
+input is in the format:
+2 8
+6 3
+3 1
+
+we store the columns in input as lists and sort them
+2 1
+3 3
+6 8
+
+then we traverse and get absolute diff and add them up
+*/
 func main() {
-	file, err := os.Open("inputs/2.txt")
+	reader := fileReader("inputs/1.txt")
+	defer reader.Close()
+
+	column1, column2 := ioColumns(reader)
+	sort.Ints(column1)
+	sort.Ints(column2)
+
+	result := 0
+	for i := range column1 {
+		result += absIntDiff(column1[i], column2[i])
+	}
+
+	fmt.Println(result)
+}
+
+// allow main function to be just logical ops and move other things to the side
+func fileReader(path string) *bufioCloser {
+	file, err := os.Open(path)
 	if err != nil {
 		fmt.Println(fmt.Errorf("open failed: %w", err).Error())
-		return
+		panic("file panic")
 	}
-	defer file.Close()
 
-	reader := bufio.NewReader(file)
+	return &bufioCloser{bufio.NewReader(file), file}
+}
 
-	var list1, list2 []int
-
+// read lines and get fields and append to 2 lists
+// first list corresponds to first column's values
+// second list corresponds to second column's values
+// conver to numbers from strings for math operations
+func ioColumns(reader *bufioCloser) (column1, column2 []int) {
+	var col1, col2 []int
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
@@ -31,21 +73,21 @@ func main() {
 		numbers := strings.Fields(line)
 
 		num1 := atoiWrap(numbers[0])
-		list1 = append(list1, num1)
+		col1 = append(col1, num1)
 
 		num2 := atoiWrap(numbers[1])
-		list2 = append(list2, num2)
+		col2 = append(col2, num2)
 	}
+	return col1, col2
+}
 
-	sort.Ints(list1)
-	sort.Ints(list2)
-
-	result := 0
-	for i := range list1 {
-		result += absIntDiff(list1[i], list2[i])
+func atoiWrap(number string) int {
+	num, err := strconv.Atoi(number)
+	if err != nil {
+		fmt.Println(fmt.Errorf("atoi failed: %w", err).Error())
+		panic("atoi panic")
 	}
-
-	fmt.Println(result)
+	return num
 }
 
 func absIntDiff(a, b int) int {
@@ -53,12 +95,4 @@ func absIntDiff(a, b int) int {
 		return a - b
 	}
 	return b - a
-}
-
-func atoiWrap(number string) int {
-	num, err := strconv.Atoi(number)
-	if err != nil {
-		fmt.Println(fmt.Errorf("atoi failed: %w", err).Error())
-	}
-	return num
 }
