2296f7d49fed658bf5fc6fdd29812319ecb4750a
Luv <luv.vermani@gmail.com>
2024-12-05 17:02:04 +0530

    adding day 5 init

---
+++ cmd/5/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type bufioCloser struct {
+	*bufio.Reader
+	io.Closer
+}
+
+func main() {
+	args := os.Args
+
+	if len(args) < 2 {
+		args = append(args, "v1")
+	}
+
+	reader := fileReader("inputs/4.txt")
+	defer reader.Close()
+
+	lines := ioConvert(reader)
+	// count := XmasCounter(lines, args[1])
+	fmt.Println(lines)
+}
+
+func fileReader(path string) *bufioCloser {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(fmt.Errorf("open failed: %w", err).Error())
+		panic("file panic")
+	}
+
+	return &bufioCloser{bufio.NewReader(file), file}
+}
+
+func ioConvert(reader *bufioCloser) [][]rune {
+	var lines [][]rune
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		line = strings.TrimSpace(line)
+		lines = append(lines, []rune(line))
+	}
+	return lines
+}
