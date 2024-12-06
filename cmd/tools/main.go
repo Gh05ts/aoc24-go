eac4f7f940e58d9fcc473693dd4cee601b7d2577
Luv <luv.vermani@gmail.com>
2024-12-06 06:13:50 +0530

    creating stubs for all 25 days

---
+++ cmd/tools/main.go
@@ -0,0 +1,40 @@
+package main
+
+func main() {
+	// for i := 7; i <= 25; i++ {
+	// dir := "cmd/" + strconv.Itoa(i)
+	// err := os.MkdirAll(dir, 0755)
+	// if err != nil {
+	// 	fmt.Println("Error creating nested directories:", err)
+	// 	return
+	// }
+	// 		fileName := dir + "/main.go"
+	// 		main, err := os.Create(fileName)
+	// 		if err != nil {
+	// 			fmt.Println("Error creating file:", err)
+	// 			return
+	// 		}
+	// 		defer main.Close()
+	// 		_, err = main.WriteString(`package main
+
+	// import (
+	// 	"fmt"
+	// )
+
+	// func main() {
+	// 	fmt.Println("hello")
+	// }
+	// `)
+	// 		if err != nil {
+	// 			fmt.Println("Error writing to file:", err)
+	// 			return
+	// 		}
+	// inputFileName := "inputs/" + strconv.Itoa(i) + ".txt"
+	// input, err := os.Create(inputFileName)
+	// if err != nil {
+	// 	fmt.Println("Error creating file:", err)
+	// 	return
+	// }
+	// defer input.Close()
+	// }
+}
