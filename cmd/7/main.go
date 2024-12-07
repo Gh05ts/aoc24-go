77a36853298ba18464ce211000397b75c40e72d5
Luv <luv.vermani@gmail.com>
2024-12-08 04:29:13 +0530

    adding day 7 solution

--- cmd/7/main.go
+++ cmd/7/main.go
@@ -1,9 +1,152 @@
 package main
-		
+
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/maps"
 )
 
+type bufioCloser struct {
+	*bufio.Reader
+	io.Closer
+}
+
 func main() {
-	fmt.Println("hello")
+	args := os.Args
+
+	if len(args) < 2 {
+		args = append(args, "v1")
+	}
+
+	reader := fileReader("inputs/7.txt")
+	defer reader.Close()
+
+	targetInputs := ioConvert(reader)
+	count := validCount(targetInputs, args[1])
+	fmt.Println(count)
+}
+
+func fileReader(path string) *bufioCloser {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(fmt.Errorf("open failed: %w", err).Error())
+		panic("file panic")
+	}
+
+	return &bufioCloser{bufio.NewReader(file), file}
+}
+
+type pair struct {
+	x, y int
+}
+
+func ioConvert(reader *bufioCloser) map[pair][]int {
+	targetInputs := make(map[pair][]int)
+	index := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		line = strings.TrimSpace(line)
+		target, inputs := stringToIntSlice(line)
+		targetInputs[pair{index, target}] = inputs
+		index++
+	}
+	return targetInputs
+}
+
+func stringToIntSlice(str string) (int, []int) {
+	stringSlice := strings.Split(str, ":")
+	target := atoiWrap(stringSlice[0])
+	intSlice := []int{}
+	inputsSlice := strings.Fields(stringSlice[1])
+	for _, str := range inputsSlice {
+		num := atoiWrap(str)
+		intSlice = append(intSlice, num)
+	}
+	return target, intSlice
+}
+
+func atoiWrap(number string) int {
+	num, err := strconv.Atoi(number)
+	if err != nil {
+		fmt.Println(fmt.Errorf("atoi failed: %w", err).Error())
+		panic("atoi panic")
+	}
+	return num
+}
+
+func validCount(targetInputs map[pair][]int, stage string) int {
+	count := 0
+	allowed := strings.Compare(stage, "v2") == 0
+	for target, inputs := range targetInputs {
+		memo := make(map[string]bool)
+		if canFormTarget(inputs, target.y, inputs[0], 1, memo, allowed) {
+			count += target.y
+		}
+		maps.Clear(memo)
+	}
+	return count
+}
+
+func canFormTarget(nums []int, target int, current int, index int, memo map[string]bool, concatAllowed bool) bool {
+	key := fmt.Sprintf("%d-%d", index, current)
+
+	if result, exists := memo[key]; exists {
+		return result
+	}
+
+	if index == len(nums)-1 {
+		if current+nums[index] == target {
+			memo[key] = true
+			return true
+		}
+		if current*nums[index] == target {
+			memo[key] = true
+			return true
+		}
+		if concatAllowed {
+			concatenated := concatenateNumbers(current, nums[index])
+			if concatenated == target {
+				memo[key] = true
+				return true
+			}
+		}
+
+		memo[key] = false
+		return false
+	}
+
+	if canFormTarget(nums, target, current+nums[index], index+1, memo, concatAllowed) {
+		memo[key] = true
+		return true
+	}
+	if canFormTarget(nums, target, current*nums[index], index+1, memo, concatAllowed) {
+		memo[key] = true
+		return true
+	}
+	if concatAllowed {
+		concatenated := concatenateNumbers(current, nums[index])
+		if canFormTarget(nums, target, concatenated, index+1, memo, concatAllowed) {
+			memo[key] = true
+			return true
+		}
+	}
+
+	memo[key] = false
+	return false
+}
+
+func concatenateNumbers(a, b int) int {
+	strA := strconv.Itoa(a)
+	strB := strconv.Itoa(b)
+	concatenatedStr := strA + strB
+	concatenated, _ := strconv.Atoi(concatenatedStr)
+	return concatenated
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gh05ts/aoc24-go
 
 go 1.20
+
+require golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d // indirect
