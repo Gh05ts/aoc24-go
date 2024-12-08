669917ebfd2dc367983090208c6df4c8624f3bf0
Luv <luv.vermani@gmail.com>
2024-12-08 20:25:26 +0530

    day 8 stage 2 init

--- cmd/8/main.go
+++ cmd/8/main.go
@@ -71,39 +71,40 @@ func findAllLocations(grid [][]rune, stage string) int {
 	height := len(grid)
 	width := len(grid[0])
 	uniqueLocations := make(map[pair]bool)
+	recursiveFlag := strings.Compare(stage, "v2") == 0
 	for _, locations := range runeLocations {
 		for i := 0; i < len(locations); i++ {
 			for j := i + 1; j < len(locations); j++ {
-				addValidLocations(locations, i, j, height, width, uniqueLocations)
+				addValidLocations(locations, i, j, height, width, uniqueLocations, recursiveFlag)
 			}
 		}
 	}
 	return len(uniqueLocations)
 }
 
-func addValidLocations(locations []pair, i, j, height, width int, uniqueLocations map[pair]bool) {
+func addValidLocations(locations []pair, i, j, height, width int, uniqueLocations map[pair]bool, loop bool) {
 	pointA := locations[i]
 	pointB := locations[j]
 
-	var point1, point2 pair
+	points := []pair{}
 	diffHeight := 0
 	diffLen := 0
 	if pointA.y > pointB.y { // diagonal
 		diffHeight = pointB.x - pointA.x
 		diffLen = pointA.y - pointB.y
-		point1 = pair{pointA.x - diffHeight, pointA.y + diffLen}
-		point2 = pair{pointB.x + diffHeight, pointB.y - diffLen}
+		points = append(points, pair{pointA.x - diffHeight, pointA.y + diffLen})
+		points = append(points, pair{pointB.x + diffHeight, pointB.y - diffLen})
 	} else {
 		diffHeight = pointB.x - pointA.x
 		diffLen = pointB.y - pointA.y
-		point1 = pair{pointA.x - diffHeight, pointA.y - diffLen}
-		point2 = pair{pointB.x + diffHeight, pointB.y + diffLen}
+		points = append(points, pair{pointA.x - diffHeight, pointA.y - diffLen})
+		points = append(points, pair{pointB.x + diffHeight, pointB.y + diffLen})
 	}
-	if isValid(point1, height, width) {
-		uniqueLocations[point1] = true
-	}
-	if isValid(point2, height, width) {
-		uniqueLocations[point2] = true
+
+	for _, point := range points {
+		if isValid(point, height, width) {
+			uniqueLocations[point] = true
+		}
 	}
 }
 
@@ -111,18 +112,8 @@ func isValid(point pair, len, wid int) bool {
 	return point.x >= 0 && point.x < len && point.y >= 0 && point.y < wid
 }
 
-func diagUpRight() {
+func ApplyOp(inp pair, x, y int) pair {
+	out := pair{}
 
-}
-
-func diagDownLeft() {
-
-}
-
-func diagUpLeft() {
-
-}
-
-func diagDownRight() {
-
+	return out
 }
