66e114701210f7fa3fc19775fc396985adc924a2
Luv <luv.vermani@gmail.com>
2024-12-22 06:14:42 +0530

    adding day 12 solution

--- cmd/12/main.go
+++ cmd/12/main.go
@@ -22,8 +22,11 @@ func main() {
 	reader := fileReader("inputs/12.txt")
 	defer reader.Close()
 
-	// grid := ioConvert(reader)
+	grid := ioConvert(reader)
+	flag := strings.Compare(args[1], "v2") == 0
 	// printGrid(grid)
+	price := totalPrice(grid, flag)
+	fmt.Println(price)
 }
 
 func fileReader(path string) *bufioCloser {
@@ -49,10 +52,133 @@ func ioConvert(reader *bufioCloser) [][]rune {
 	return lines
 }
 
+type pair struct {
+	x, y int
+}
+
+func totalPrice(grid [][]rune, discount bool) int {
+	price := 0
+	visited := make(map[pair]bool)
+	groups := [][]pair{}
+
+	LenLimit := len(grid)
+	BreadthLimit := len(grid[0])
+	for i := range grid {
+		for j := range grid[i] {
+			if !visited[pair{i, j}] {
+				currentGroup := new([]pair)
+				dfs(grid, i, j, LenLimit, BreadthLimit, currentGroup, visited)
+				groups = append(groups, *currentGroup)
+			}
+		}
+	}
+
+	fourDirections := [4][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	for i := range groups {
+		perimeter := 0
+		corners := 0
+		for _, val := range groups[i] {
+			curChar := grid[val.x][val.y]
+			for _, direction := range fourDirections {
+				ni, nj := val.x+direction[0], val.y+direction[1]
+				if ni >= 0 && ni < LenLimit && nj >= 0 && nj < BreadthLimit && grid[ni][nj] != curChar {
+					perimeter += 1
+				} else if ni < 0 || nj < 0 || ni == LenLimit || nj == BreadthLimit {
+					perimeter += 1
+				}
+			}
+
+			corners += countCorners(groups[i], val)
+		}
+		// fmt.Println("i: ", i, "area:", len(groups[i]), "perimeter:", perimeter, "corners:", corners)
+		if discount {
+			price += len(groups[i]) * corners
+		} else {
+			price += len(groups[i]) * perimeter
+		}
+	}
+
+	return price
+}
+
+func countCorners(group []pair, cur pair) int {
+	corners := 0
+	sevenDirection := [7][2]int{{-1, 0}, {0, -1}, {-1, -1}, {1, 0}, {1, -1}, {0, 1}, {-1, 1}}
+
+	x, y := cur.x, cur.y
+	north := pair{x + sevenDirection[0][0], y + sevenDirection[0][1]}
+	west := pair{x + sevenDirection[1][0], y + sevenDirection[1][1]}
+	northWest := pair{x + sevenDirection[2][0], y + sevenDirection[2][1]}
+	south := pair{x + sevenDirection[3][0], y + sevenDirection[3][1]}
+	southWest := pair{x + sevenDirection[4][0], y + sevenDirection[4][1]}
+	east := pair{x + sevenDirection[5][0], y + sevenDirection[5][1]}
+	northEast := pair{x + sevenDirection[6][0], y + sevenDirection[6][1]}
+
+	if !search(group, north) {
+		sameEdge := search(group, west) && !search(group, northWest)
+		if !sameEdge {
+			corners += 1
+		}
+	}
+
+	if !search(group, south) {
+		sameEdge := search(group, west) && !search(group, southWest)
+		if !sameEdge {
+			corners += 1
+		}
+	}
+
+	if !search(group, west) {
+		sameEdge := search(group, north) && !search(group, northWest)
+		if !sameEdge {
+			corners += 1
+		}
+	}
+
+	if !search(group, east) {
+		sameEdge := search(group, north) && !search(group, northEast)
+		if !sameEdge {
+			corners += 1
+		}
+	}
+	return corners
+}
+
+func search(haystack []pair, needle pair) bool {
+	for _, hay := range haystack {
+		if needle == hay {
+			return true
+		}
+	}
+	return false
+}
+
+func dfs(grid [][]rune, i, j, length, breadth int, groups *[]pair, visited map[pair]bool) {
+	visited[pair{i, j}] = true
+	*groups = append(*groups, pair{i, j})
+
+	directions := [4][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	for _, dir := range directions {
+		ni, nj := i+dir[0], j+dir[1]
+		if ni >= 0 && ni < length && nj >= 0 && nj < breadth && grid[ni][nj] == grid[i][j] && !visited[pair{ni, nj}] {
+			dfs(grid, ni, nj, length, breadth, groups, visited)
+		}
+	}
+}
+
 // func printGrid(grid [][]rune) {
 // 	for i, _ := range grid {
 // 		for j, _ := range grid[i] {
 // 			fmt.Print(string(grid[i][j]))
+// 		}
+// 		fmt.Println()
+// 	}
+// }
+
+// func printGroups(grid [][]rune, groups [][]pair) {
+// 	for i := range groups {
+// 		for _, val := range groups[i] {
+// 			fmt.Print(string(grid[val.x][val.y]))
 // 		}
 // 		fmt.Println()
 // 	}
